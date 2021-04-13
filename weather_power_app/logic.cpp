32470fd6df49a712ee50bb6799a3b473ff7299a3
Eero Eriksson <eero.eriksson@tuni.fi>
2021-04-13 16:53:51 +0300

    add: preferences can be requisted

--- weather_power_app/logic.cpp
+++ weather_power_app/logic.cpp
@@ -9,7 +9,7 @@ Logic::Logic()
 void Logic::init(){
 
     preference_ = new Preference();
-    preference_->create_save_file();
+    // preference_->create_save_file();
     data_ = new Data();
     w_.show();
     connect(&w_, &MainWindow::sendDateInformation, this, &Logic::getDataTimes);
